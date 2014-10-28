69e48ab1b189ae8071ded2c1bcd74074fd87c5c3
Eugene Matsak <matsak@cedrus.com>
2014-10-28 15:35:08 -0700

    Upon reflection, these two functions are best left out of the shared repo.

--- scons/CedrusAutoversioningHelpers.py
+++ scons/CedrusAutoversioningHelpers.py
@@ -1,26 +1,8 @@
 #!/usr/bin/env python
 
-__author__  = "$Author: kjones $"
-__version__ = "$Revision: 7073 $"
-__date__    = "$Date: 2010-08-02 13:02:43 -0700 (Mon, 02 Aug 2010) $"
-
-import os, sys, time
-
-
-import time
-import datetime
-import re # regular expressions
-import SCons
+import os, time
 
 from subprocess import *
-
-def get_current_version_suffix( path_to_scons_dir, version_file ):
-    text_version_file = path_to_scons_dir + os.sep + version_file
-    sock = open(text_version_file,"r")
-    sock.readline().rstrip(' \n') # throw away first line
-    version = sock.readline().rstrip(' \n')
-    sock.close()
-    return version
 
 def get_git_version():
     git_version = "ABCDEFGHIJKLM"
@@ -35,52 +17,8 @@ def get_git_version():
     git_version = git_version[:8] # take only the first 8 char of the git commit hash
 
     return git_version
-    
-def DeclareAutoversioningBuildDuties( env, header_path, version_file_path ):
 
-    # This creates [repo_original_source_files]/header_path/AutoversionHeader.h
-    def PopulateVersionHeaderWithCurrentDateAndCommitHash( target, source, env ):
-
-        path_that_sits_inside_source_repo = env.subst( header_path )
-
-        # IMPORTANT! do NOT make changes to how we create AutoversionHeader without
-        # giving CAREFUL CONSIDERATION to potential side-effects on class DeveloperHeaderForDiskFile
-        run(
-            path_that_sits_inside_source_repo, # where to create the header file
-            'AutoversionHeader.h', # name of file to create
-            source[0].abspath, # input file (SuperLab Current Version.txt)
-            )
-
-        return None
-
-    path_to_av_header = env.subst( header_path )
-    input_file_for_autoversion_header = env.subst( version_file_path )
-
-    autoversion_header_action = env.Command(
-        path_to_av_header + '/AutoversionHeader.h',  # the RESULTANT OUTPUT of this command
-        input_file_for_autoversion_header,
-        PopulateVersionHeaderWithCurrentDateAndCommitHash
-        )
-
-    # this is here because the other calls that use this path won't
-    # work until the path has been CREATED
-    env.Depends( autoversion_header_action, env['CEDRUS_COMPLETE_THIRD_PARTY_STAGING'] )
-
-    # I thought SCons would figure out on its own to put this in
-    # OBJ_ROOT, but it didn't.  There could be better ways to handle
-    # all the stuff for AutoversionHeader.h, but this works reliably.
-    copied_autoversion_header_action = env.Command(
-        '$OBJ_ROOT/AutoversionHeader.h',
-        autoversion_header_action,
-        SCons.Script.Copy("$TARGET", "$SOURCE")
-        )
-
-    # this is here because the other calls that use this path won't
-    # work until the path has been CREATED
-    env.Depends( copied_autoversion_header_action, env['CEDRUS_COMPLETE_THIRD_PARTY_STAGING'] )
-
-
-def run(path,outfile,input_file):
+def run_autoversioning(path,outfile,input_file):
     sock = open(input_file,"r")
     current_version = sock.readline().rstrip(' \n')
     sock.close()
