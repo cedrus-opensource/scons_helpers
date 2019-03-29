4b440b768f6d98b4fc1ef16495dea0474fbb4603
Eugene Matsak <matsak@cedrus.com>
2019-03-29 15:09:56 -0700

    Making the openssl script use the freshly committed dylibs.

--- scons/OpenSSL.py
+++ scons/OpenSSL.py
@@ -15,7 +15,7 @@ def need_openssl(env):
 
 def publish_all_libs_to_staging(env):
     if sys.platform == 'darwin':
-        return []
+        return publish_all_libs_to_staging_mac(env)
     elif sys.platform == 'win32':
         return publish_all_libs_to_staging_win(env)
     elif sys.platform == 'linux2':
@@ -24,22 +24,35 @@ def publish_all_libs_to_staging(env):
         raise ValueError('Unknown Operating System')
 
 def need_openssl_mac(env):
-    lib_dependencies = [ 'ssl', 'crypto' ]
-    include_path = [ '-I/usr/local/opt/openssl/include' ]
-    libpath = [ '/usr/local/opt/openssl/lib' ]
+    lib_dependencies = ['ssl', 'crypto']
+    include_path = ['-I/' + env['OPENSSL']+'/darwin/include/']
+    libpath = [env['OPENSSL']+'/darwin/lib']
 
     env.AppendUnique( CXXFLAGS = include_path,
                 LIBPATH = libpath,
                 LIBS = lib_dependencies, )
 
 def publish_all_libs_to_staging_win(env):
-    # publish openssl libraries
-    ssl_libs = env.Glob( env['OPENSSL']+'/bin/*.dll' )
+    ssl_libs = env.Glob(env['OPENSSL']+'/bin/*.dll')
 
     results = []
 
     for lib in ssl_libs:
-        results += env.Install( '$STAGING_DIR', lib )
+        results += env.Install('$STAGING_DIR', lib)
+
+    return results
+
+def publish_all_libs_to_staging_mac(env):
+    ssl_loc_prefix = env['OPENSSL']+'/darwin/lib/'
+
+    exclude = [ssl_loc_prefix+'libssl.dylib', ssl_loc_prefix+'libcrypto.dylib']
+    ssl_libs = env.Glob(ssl_loc_prefix+'*.dylib', exclude=exclude)
+
+    results = []
+
+    for lib in ssl_libs:
+        results += env.Install('$STAGING_DIR', lib)
+        results += env.Install(env.subst('$STAGING_DIR/') + env['APP_BUNDLE_NAME'] + '.app/Contents/MacOS/', lib)
 
     return results
 
